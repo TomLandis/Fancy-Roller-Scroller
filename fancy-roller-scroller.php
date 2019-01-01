1345fb705f5d87ff01cced24647d6a0d4321028a
Tom Landis <Tomhearsu@outlook.com>
2019-01-01 15:52:51 +0900

    Update fancy-roller-scroller.php

--- fancy-roller-scroller.php
+++ fancy-roller-scroller.php
@@ -17,28 +17,39 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 
-/** Step 2 (from text above). */
+/** Create Menu */
 add_action( 'admin_menu', 'my_plugin_menu' );
 
-/** Step 1. */
+
 function my_plugin_menu() {
 	add_options_page( 'My Plugin Options', 'Fancy Roller Scroller', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
 }
+/** Save Initial values of list into database */
+function activateFancyRoller(){
+	add_option( 'ListOfStuff', ['Take a look at', 'this', 'that', 'all the stuff']);
+}
 
-/** Step 3. */
+register_activation_hook( __FILE__, 'activateFancyRoller');
+/** pull details from DB */
 function my_plugin_options() {
 	if ( !current_user_can( 'manage_options' ) )  {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
+	$listo = get_option('ListOfStuff');
+	$theNum = count($listo);
+	$first = $listo[0];
 	echo '<script type="text/javascript" src="'.plugin_dir_url( __FILE__ ).'js/fancy-settings.js"></script>';
 	echo '<p>Your list must have<strong> at least</strong> two items. When you like the way things look, press save.</p>
 	<button id="saveChanges">SAVE</button>';
-	echo '<h3><input id="aboveText" value="things that hurt me"> &#11013; Text above rolling list</h3>';
+	echo '<h3><input id="aboveText" value="'.$first.'"> &#11013; Text above rolling list</h3>';
 	echo '<div id="list-wrap">';
-	echo '<p><input id="item-1" value="sticks"></input> <label for="item-1">#1 Item</p>';
-	echo '<p><input id="item-1" value="stones"></input> <label for="item-1">#2 Item</p>';
+	for($i=1;$i< $theNum;$i++){
+		echo '<p><input id="item-'.$i.'" value="'.$listo[$i].'"></input> <label for="item-'.$i.'">#'.$i.' Item</p>';
+	}
+	
+	
 	echo '</div>';
 	echo '<button id="addItemButton">Add Another item to the list</button>';
 	
 }
-
+/** The next step is to make the save button write the options to the database */
